12ceaa7435a59829a5e0b6e7f4027b29a374b9f5
zungf <41270843+zungf@users.noreply.github.com>
2018-07-31 06:13:45 +0100

    Create audio.py

---
+++ audio.py
@@ -0,0 +1,169 @@
+
+Save New Duplicate & Edit Just Text Twitter
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+import discord
+import asyncio
+import youtube_dl
+import os
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+
+bot=commands.Bot(command_prefix='-')
+
+from discord import opus
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(opus_libs)))
+load_opus_lib()
+
+
+
+@bot.event
+async def on_ready():
+    print("hi")
+opts = {
+            'default_search': 'auto',
+            'quiet': True,
+        }    
+    
+@bot.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+
+
+
+players={}
+@bot.command(pass_context=True)
+async def play(ctx, *,url):
+    global play_server
+    play_server = ctx.message.server
+    voice = bot.voice_client_in(play_server)
+    global player
+    player = await voice.create_ytdl_player(url,ytdl_options=opts)
+    players[play_server.id] = player
+    if player.is_live == True:
+        await bot.say("Can not play live audio yet.")
+    elif player.is_live == False:
+        player.start()
+
+
+async def pause(ctx):
+    player.pause()
+
+@bot.command(pass_context=True)
+async def resume(ctx):
+    player.resume()
+          
+@bot.command(pass_context=True)
+async def volume(ctx, vol):
+    vol = float(vol)
+    vol = player.volume = vol
+
+@bot.command(pass_context=True)
+async def stop(ctx):
+    server=ctx.message.server
+    voice_client=bot.voice_client_in(server)
+    await voice_client.disconnect()
+
+
+
+bot.run(os.environ['BOT_TOKEN'])
